ca6b450a0ec534a7259bd869da3619e95500fab2
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-06-22 13:34:43 +0530

    03_variables

---
+++ C-Programs/03_variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a=6;//declaration + initialization ek saath
+           //ye shortcut hai — alag alag karne ki zaroorat nahi
+    printf("the output of this program is %d",a);
+    return 0;
+
+}
