bf98e44b647580ecf175b392493fc61f9de9146a
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-06-27 08:07:29 +0530

    05_problem.c

---
+++ C/chapter_02_practice_set/05_problem.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    float a = 3.0 + 1;
+    printf("the value of a is %f",a);
+    return 0;
+}
+/* 3.0 + 1 will be:
+Floating point number because 
+An Arithmetic operation between
+• int and int → int
+• int and float → float
+• float and float → float
+*/
