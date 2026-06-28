80d3d5e16a5a1187f6b3cc84ba3af4b36c34ace7
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-06-28 06:30:52 +0530

    02_if_else.c

---
+++ C/chapter_03/02_if_else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int age = 5;
+    if(age>10){
+        printf("we are inside if\n");
+        printf("your age is greater than 11\n");
+    }
+    else{
+        printf("your age is note greater than 10");
+    }
+    return 0;
+}
+/*
+if (condition_to_be_checked) {
+// Statements if condition is true
+} else {
+// Statements if condition is false
+}
+*/
+//Note that - else block is not necessary but optional.
