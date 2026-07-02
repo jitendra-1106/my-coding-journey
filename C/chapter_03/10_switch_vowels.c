212340a93e4addae5b01ea3a2accca0e54edd3b0
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-07-02 14:47:22 +0530

    chapter_03 completed

---
+++ C/chapter_03/10_switch_vowels.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    char letter;
+    printf("Enter letter:");
+    scanf("%c",&letter);
+
+    switch(letter){
+        case 'a':
+        printf("a is vowel");
+        break;
+        case 'e':
+        printf("e is vowel");
+        break;
+        case 'i':
+        printf("i is vowel");
+        break;
+        case 'o':
+        printf("o is vowel");
+        break;
+        case 'u':
+        printf("u is vowel");
+        break;
+        default:
+        printf("%c is not vowel",letter);
+    }
+    return 0;
+}
