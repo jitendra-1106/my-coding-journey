885614c4bf3f8e2ed75303e73cb1db99c1a9f926
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-07-05 11:43:08 +0530

    01-loop.c

---
+++ C/chapter_04/01_loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    // problem : Print Happy Birthday 10 times
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    printf("Happy Birthday !!\n");
+    return 0;
+}
+
+
+// A loop is used to execute the same block of code repeatedly until a condition is met.OR
+ // Loops are used to repeat the same block of code multiple times.
+ // They help avoid writing the same statements again and again.
+ // Loops make programs shorter, cleaner, and easier to maintain.
