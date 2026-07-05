c08256915360b585de7c6fddd10e308ca48ce649
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-07-05 11:57:30 +0530

    03_while_value.c

---
+++ C/chapter_04/03_while_value.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    // The loop variable is also called the loop counter.
+    int i=0;
+
+     // The condition is checked before every iteration.
+    while(i<11){
+        printf("The value of i is %d\n",i);
+        i++;
+    }
+    return 0;
+}
