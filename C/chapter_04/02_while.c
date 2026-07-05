be931248b3e161c27cfd94d044346ea2a57b2f1a
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-07-05 11:51:36 +0530

    02_while.c

---
+++ C/chapter_04/02_while.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int i=0;
+
+     // The loop runs as long as the condition is true.
+    // As soon as the condition becomes false, the loop terminates.
+    while(i<10){
+        printf("Happy Birthday !!\n");
+        i=i+1;    //i++
+    }
+    return 0;
+}
