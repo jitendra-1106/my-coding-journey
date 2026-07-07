daabb9ad4e1301c77a08d0e9d17f7b489cea27f7
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-07-07 07:13:43 +0530

    07_do_while.c

---
+++ C/chapter_04/07_do_while.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//do-while loop = while loop which executes at least once
+int main(){
+    int i=5;
+    do
+    {
+        printf("The value of i is %d\n",i);
+    } while(i<3);
+    
+    return 0;
+}
