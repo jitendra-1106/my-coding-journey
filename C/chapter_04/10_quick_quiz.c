c1f3f88cabef9b0708627c1169c442becec60996
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-07-08 21:57:46 +0530

    10_quick_quiz.c

---
+++ C/chapter_04/10_quick_quiz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+//Quick Quiz: Write a program to print first ‘n’ natural numbers using for loop.
+
+{
+    int n;
+    printf("Enter the number n:");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
