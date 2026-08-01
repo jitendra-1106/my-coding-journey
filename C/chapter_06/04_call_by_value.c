0f83a181b02348fcecd1723f469eea10ada0c836
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-08-01 22:43:27 +0530

    04_call_by_value.c

---
+++ C/chapter_06/04_call_by_value.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sum(int ,int);
+
+int sum(int a,int b){
+    return a+b;  // Call by Value: values of x and y are copied into a and b
+}
+
+int main(){
+    int x=1 , y=6;
+    printf("The sum of 1 and 6 is %d",sum(x,y));
+
+    return 0;
+}
