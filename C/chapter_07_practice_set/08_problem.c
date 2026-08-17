43424ca1f7dd5b769238c848efad85198033a24f
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-08-17 13:55:27 +0530

    07_problem.c

--- C/chapter_07_practice_set/08_problem.c
+++ C/chapter_07_practice_set/08_problem.c
@@ -3,32 +3,6 @@
 int main()
 {
     int arr[3][10];
-    int mul[] = {2, 7, 9};
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            arr[i][j] = mul[i] * (j + 1);
-        }
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 10; j++)
-        {
-            printf("%d\t", arr[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
-
-/*
-#include <stdio.h>
-
-int main()
-{
-    int arr[3][10];
 
     for (int i = 0; i < 3; i++)
     {
@@ -56,4 +30,3 @@ int main()
 
     return 0;
 }
-*/
