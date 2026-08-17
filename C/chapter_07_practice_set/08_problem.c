b4eb6e59a85ebead9987060a6181e6be58d2d275
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-08-17 14:07:54 +0530

    08_problem.c

--- C/chapter_07_practice_set/08_problem.c
+++ C/chapter_07_practice_set/08_problem.c
@@ -2,6 +2,38 @@
 
 int main()
 {
+    int n1, n2, n3;
+    scanf("%d %d %d", &n1, &n2, &n3);
+    int arr[3][10];
+    int mul[] = {n1, n2, n3};
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            arr[i][j] = mul[i] * (j + 1);
+        }
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2, n3;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    scanf("%d", &n3);
     int arr[3][10];
 
     for (int i = 0; i < 3; i++)
@@ -9,13 +41,13 @@ int main()
         for (int j = 0; j < 10; j++)
         {
             if (i == 0)
-                arr[i][j] = 2 * (j + 1);
+                arr[i][j] = n1 * (j + 1);
 
             if (i == 1)
-                arr[i][j] = 7 * (j + 1);
+                arr[i][j] = n2 * (j + 1);
 
             if (i == 2)
-                arr[i][j] = 9 * (j + 1);
+                arr[i][j] = n3 * (j + 1);
         }
     }
 
@@ -30,3 +62,4 @@ int main()
 
     return 0;
 }
+*/
