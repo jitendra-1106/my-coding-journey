4023808fa0c1af48e472b42dd48167c2e8bdc55c
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-08-23 14:19:21 +0530

    03_problem.c

---
+++ C/chapter_09_practice_set/03_problem.c
@@ -0,0 +1,17 @@
+/*  
+Twenty integers are to be stored in memory. What will you prefer- Array or
+structure?
+*/
+
+/*
+If twenty integers are to be stored in memory, I would prefer an array because all 20 values are of the same data type (int).
+
+int arr[20];
+*/
+
+#include <stdio.h>
+
+int main(){
+    int arr[20];
+    return 0;
+}
