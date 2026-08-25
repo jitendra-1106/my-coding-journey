68cf2cc1d8b62170819d35ba3609aecabcbabb34
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-08-25 12:30:33 +0530

    04_fgetc_fputc.c

---
+++ C/chapter_10/04_fgetc_fputc.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *ptr;
+
+    // ptr = fopen("harry.txt", "r");
+    // char c = fgetc(ptr); // used to read a character from file
+    // printf("%c", c);
+
+    ptr = fopen("harry.txt", "w");
+    fputc('c', ptr); // used to write character 'c' to the file
+    return 0;
+}
