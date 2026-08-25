89d20ccf1c725b0cdd7858b1865338c576ae415e
Jitendra kumar <kumar1106jitendra@gmail.com>
2026-08-25 20:20:34 +0530

    03_problem.c

---
+++ C/chapter_11_practice_set/03_problem.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n = 5;
+    int *ptr;
+
+    ptr = (int *)calloc(n , sizeof(int));
+
+    ptr[0] = 4;
+    ptr[1] = 8;
+    ptr[2] = 6;
+    ptr[3] = 3;
+    ptr[4] = 5;
+
+    printf("%d\n", ptr[0]);
+    printf("%d\n", ptr[1]);
+    printf("%d\n", ptr[2]);
+    printf("%d\n", ptr[3]);
+    printf("%d\n", ptr[4]);
+    return 0;
+}
