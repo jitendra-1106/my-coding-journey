21e20448e6c67b6090753a1386d740d2b25ca8d3
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-16 23:20:04 +0530

    chapter_00_completed

---
+++ C++/chapter_00/hello_world.cpp
@@ -0,0 +1,23 @@
+#include <iostream> // Includes the iostream library for input and output
+
+int main()
+{ // Program execution starts from main()
+
+    std::cout << "HELLO WORLD"; // Prints "HELLO WORLD" on the screen
+                                // cout is used for output
+                                // std:: is the namespace prefix for the standard library
+                                // << sends the text to cout
+
+    return 0; // Ends the main() function successfully
+}
+
+/*
+#include <iostream>
+using namespace std;  // Allows us to use names from the std namespace
+                      // without writing std:: every time
+
+int main(){
+    cout<<"HELLO WORLD";
+    return 0;
+}
+*/
