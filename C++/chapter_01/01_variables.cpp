be2744293c973bf7a17641e3e121234353103a92
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-16 23:33:12 +0530

    01_variables.cpp

---
+++ C++/chapter_01/01_variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int sum = 7;  // Creates an integer variable named sum and stores 7 in it
+
+    cout << "HELLO WORLD" << sum; // Multiple values can be sent continuously using <<
+
+    return 0;
+}
