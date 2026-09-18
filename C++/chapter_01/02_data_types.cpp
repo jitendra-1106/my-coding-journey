4100b81603a0da22d2ea1c3e7e662d97487ebfeb
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-18 15:43:58 +0530

    02_data_types.cpp

---
+++ C++/chapter_01/02_data_types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // int a = 7;
+    // int b = 8;
+    int a = 7, b = 8;
+    float pi = 3.14;
+    char c = 'j';
+
+    cout << "The value of a is " << a << "\nThe value of b is " << b;
+    cout << "\nThe value of pi is " << pi;
+    cout << "\nThe value of c is " << c;
+
+    return 0;
+}
