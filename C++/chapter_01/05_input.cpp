f790792029ada50314d3606ffbcb44e19306615e
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-18 16:30:22 +0530

    05_input.cpp

---
+++ C++/chapter_01/05_input.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1 , num2;
+
+    cout<<"Enter the value of num1:\n";
+    cin>>num1;
+
+    cout<<"Enter the value of num2:\n";
+    cin>>num2;
+
+    cout<<"The value of num1 is "<<num1<<"\nThe value of num2 is "<<num2;
+    return 0;
+}    
+
+/*
+'<<' is called Insertion operator.
+'>>' is called Extraction operator.
+*/
