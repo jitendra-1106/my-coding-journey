b2df77804f27d60eed6e6b9abd7bfbab8d93f97e
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-19 19:18:20 +0530

    chapter_02_completed

---
+++ C++/chapter_02/08_operator_precedence.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 4 , b = 7;
+    int c = ((((a*45)+b)-45)+3);
+
+    return 0;
+}
+
+/*
+// Operator Precedence:
+Determines which operator is performed first when different operators are present in an expression.
+
+// Operator Associativity:
+Determines the direction of evaluation (left-to-right or right-to-left) when operators have the same precedence.
+*/
