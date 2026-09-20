e67d2cde04a0251961e64ed5c5a483dc2611fd28
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-20 13:46:44 +0530

    04_do_while.cpp

---
+++ C++/chapter_03/04_do_while.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+/*Syntax:
+initialization;
+do{
+    C++ statements;
+    updation;
+}while(condition);
+*/
+
+int main(){
+    int i = 0;
+    do
+    {
+        cout<<i<<endl;
+    } while (i>5);
+    
+    return 0;
+}
