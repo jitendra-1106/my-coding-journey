243988085f1179604155435493df10784149df6c
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-20 13:50:03 +0530

    05_for.cpp

---
+++ C++/chapter_03/05_for.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+/*Syntax : 
+for(initialization; condition; updation){
+    loop body(C++ code);
+}
+*/
+
+int main(){
+   
+    for (int i = 0; i <=5; i++)
+    {
+        cout<<i<<endl;
+    }
+    
+    return 0;
+}
