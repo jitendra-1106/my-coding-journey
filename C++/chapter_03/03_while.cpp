59c0dc98d52b2529bec8988684d7297733552a7e
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-20 13:42:27 +0530

    03_while.cpp

---
+++ C++/chapter_03/03_while.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+/* Syntax:
+initialization;
+while(condition){
+    C++ statements;
+    updation;
+}*/
+int main(){
+    int i = 0;
+    while (i<=5)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    
+    return 0;
+}
