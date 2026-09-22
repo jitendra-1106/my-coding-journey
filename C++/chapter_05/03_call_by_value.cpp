b16d63f05f05c9528a5c3e0982931e3845ecb277
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-22 18:19:40 +0530

    03_call_by_value.cpp

---
+++ C++/chapter_05/03_call_by_value.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void swap(int a, int b);
+
+void swap(int a, int b){ // a and b are copies of x and y.
+    int temp = a;
+    a = b;
+    b = temp;
+
+    // Only the copies are swapped, not the original x and y.
+}
+
+int main(){
+    int x = 4, y = 5;
+
+    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;
+
+    swap(x, y); //This will not swap a and b.
+    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;
+    
+    return 0;
+}
