d57ec4dd402f8f30a0ad9ef8d2e8671f817379f4
jitendra-1106 <kumar1106jitendra@gmail.com>
2026-09-25 18:05:41 +0530

    03_memory_allocation.cpp

---
+++ C++/chapter_06/03_memory_allocation.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class shop{
+    int itemid[100]; // Stores IDs of up to 100 items
+    int itemprice[100]; // Stores prices of up to 100 items
+    int counter; // Stores the current array index
+
+    public:
+    void initcounter(void){ counter = 0;} // Start from index 0
+    void setprice(void);
+    void displayprice(void);
+};
+
+void shop :: setprice(void){
+
+    // counter + 1 is used only to show the item number to the user.
+    cout<<"Enter id of your item no "<<counter+1<<endl;
+
+    // counter's value is used as the array index
+    cin>>itemid[counter];
+
+    cout<<"Enter price of your item"<<endl;
+    // Store the price at the same index
+    cin>>itemprice[counter];
+
+    // Move to the next array index
+    counter++;
+}
+
+void shop :: displayprice(void){
+    // Print all items that have been stored
+    for (int i = 0; i < counter; i++)
+    {
+        cout<<"The price of item with id "<<itemid[i]<<" is "<<itemprice[i]<<endl;
+    }
+    
+}
+int main(){
+    shop dukaan;
+    // Set counter to 0
+    dukaan.initcounter();
+    // counter = 0 → itemid[0], itemprice[0]
+    dukaan.setprice();
+    // counter = 1 → itemid[1], itemprice[1]
+    dukaan.setprice();
+    // counter = 2 → itemid[2], itemprice[2]
+    dukaan.setprice();
+
+    dukaan.displayprice();
+    
+    return 0;
+}
